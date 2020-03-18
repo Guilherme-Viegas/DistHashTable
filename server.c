b8c1851e3ddadba76798202d778d0da88b660882
Guilherme-Viegas <ggmviegas@gmail.com>
2020-03-18 11:27:45 +0000

    initial

--- server.c
+++ server.c
@@ -19,7 +19,7 @@
 #endif
 
 
-int fd,errcode, newfd, client_sockets[8];
+int fd,errcode, newfd, client_sockets[3];
 int maxfd, counter;
 ssize_t n;
 fd_set rfds;
@@ -31,37 +31,24 @@ char str[100] = "";
 
 
 void createServer(Server* server) {
-  fd=socket(AF_INET,SOCK_STREAM,0);
-  if (fd==-1) {
-      printf("SOCKET ERROR\n");
-      exit(1); //error
-  }
+  fd=socket(AF_INET,SOCK_STREAM,0); //TCP socket
+  if (fd==-1) exit(1); //error
 
   memset(&hints,0,sizeof hints);
-  hints.ai_family=AF_INET;
-  hints.ai_socktype=SOCK_STREAM;
+  hints.ai_family=AF_INET; //IPv4
+  hints.ai_socktype=SOCK_STREAM; //TCP socket
   hints.ai_flags=AI_PASSIVE;
 
   errcode=getaddrinfo(NULL,server->myPort,&hints,&res);
-    if((errcode)!=0) {
-      printf("GETADDRINFO ERROR\n");
-      /*error*/exit(1);
-    }
+  if((errcode)!=0)/*error*/exit(1);
 
-  n = bind(fd,res->ai_addr,res->ai_addrlen);
-  if(n == -1){
-      printf("Error code: %d\n", errno);
-      printf("BIND ERROR\n");
-       /*error*/ exit(1);
-    }
+  n=bind(fd,res->ai_addr,res->ai_addrlen);
+  if(n==-1) /*error*/ exit(1);
 
   if(listen(fd,5)==-1)/*error*/exit(1);
 
-
   while(1) {
-    
-    printf("\nHERE\n");
-
+    printf("Linha 51\n");
 
     // Set all sockets to 0
     FD_ZERO(&rfds);
@@ -70,179 +57,120 @@ void createServer(Server* server) {
     FD_SET(fd, &rfds);
     maxfd = fd;
 
-    FD_SET(STDIN_FILENO  , &rfds); 
-
+    FD_SET(STDIN_FILENO  , &rfds);
 
     // Add all available child sockets to set
-    for (int i = 0; i < 6; i++) {
-      newfd = client_sockets[i];
-      // If there are available sockets
-      if(newfd > 0) {
-        FD_SET(newfd, &rfds);
-      }
-      // Get the highest file descriptor number
-      if(newfd > maxfd) {
-        maxfd = newfd;
-      }
+    // If there are available sockets
+    if(client_sockets[0] > 0) {
+      FD_SET(client_sockets[0], &rfds);
     }
-    if(server->prevConnFD > 0) {
-      client_sockets[6] = server->prevConnFD;
-      FD_SET(server->prevConnFD, &rfds);
+    maxfd = max(client_sockets[0], maxfd);
 
-      if(server->prevConnFD > maxfd) {
-        maxfd = server->prevConnFD;
-      }
+    if(server->prevConnFD > 0) {
+      client_sockets[1] = server->prevConnFD;
+      FD_SET(server->prevConnFD, &rfds);
     }
+    maxfd = max(server->prevConnFD, maxfd);
 
     if(server->nextConnFD > 0) {
-      client_sockets[7] = server->nextConnFD;
+      client_sockets[2] = server->nextConnFD;
       FD_SET(server->nextConnFD, &rfds);
+    }
+    maxfd = max(server->nextConnFD, maxfd);
 
-      if(server->nextConnFD > maxfd) {
-        maxfd = server->nextConnFD;
-      }
-    }   
-
+    printf("Linha 81\n");
     counter = select(maxfd + 1, &rfds, (fd_set*)NULL, (fd_set*)NULL, (struct timeval *)NULL);
-    if(counter <= 0) exit(1);
-
-    else if (FD_ISSET(STDIN_FILENO , &rfds)) { // Read from keyboard
+    if(counter <=0) exit(1);
+    if(FD_ISSET(STDIN_FILENO , &rfds)) {
       n = read(STDIN_FILENO, buffer, 128);
       if (n > 0) {
-        if(strstr(buffer, "leave") == 0) {
+        if(strstr(buffer, "leave") != NULL) {
           break;
+        } else if(strstr(buffer, "send") != NULL) {
+          n = write(server->prevConnFD, "Prev\n", 6);
+          n = write(server->nextConnFD, "Next\n", 6);
         }
       }
-    } 
+      continue;
+    }
+    printf("Linha 96\n");
 
-    // If something is happening in the main socket, it means there's a new connection 
     if(FD_ISSET(fd, &rfds)) {
-      printf("\nGive me some connections baby!\n");
+      printf("\nGive me some connections baby\n");
       addrlen = sizeof(addr);
       if((newfd = accept(fd, (struct sockaddr*)&addr, &addrlen)) == -1) exit(1);
                  //inform user of socket number - used in send and receive commands  
       printf("New connection , socket fd is %d , ip is : %s , port : %d \n" , newfd , inet_ntoa(addr.sin_addr) , ntohs (addr.sin_port));   
 
-      for(int i = 0; i < 8; i++) {
-        if(client_sockets[i] == 0) {
-          client_sockets[i] = newfd;
-          break;
-        }
-        if(i == 5) {
+        if(client_sockets[0] == 0) {
+          client_sockets[0] = newfd;
+        } else {
           close(newfd);
         }
-      }
     }
 
-    for (int i = 0; i < 6; i++) {
-      if ((client_sockets[i] == server->nextConnFD) || (client_sockets[i] == server->prevConnFD)) {
-        client_sockets[i] = 0;
-      }
+    //Prevents from reading the same port 2 times (newfd and some prev or next)
+    if ((client_sockets[0] == server->nextConnFD) || (client_sockets[0] == server->prevConnFD)) {
+        client_sockets[0] = 0;
+    }
+
+    //If only 2 servers and prev and next are the same, only read from one of them
+    if(server->nextConnFD == server->prevConnFD) {
+      client_sockets[2] = 0;
     }
 
     //Else its on some other socket
-    for(int i = 0; i < 8; i++) { 
+    for(int i = 0; i < 3; i++) {
       newfd = client_sockets[i];
-      printf("Reached for %d %d\n", i, client_sockets[i]);
-      if(i == 7) {
-        if(client_sockets[6] == client_sockets[7]) {
-          break;
-        }
-      }
       if(FD_ISSET(newfd, &rfds)) {
         if((n = read(newfd, buffer, 128)) == 0) {
           close(newfd);
           printf("Connection closed %d\n\n", i);
           client_sockets[i] = 0;
-        } else {
+        } else { 
           buffer[n] = '\0';
           write(1,"received: ",11); write(1,buffer,n); // Print incoming message
           //Now check what the incoming message is asking for
-          if(strstr(buffer, "NEW") != NULL) { //A new server is trying to enter the ring
-            serverIsEntering(buffer, &newfd, server);
+          if(strstr(buffer, "NEW") != NULL) {
+            if((server->nextConnFD == 0) && (server->prevConnFD == 0)) { //If there's only 1 server in the ring
+              server->prevConnFD = newfd;
+              server->doubleNextKey = server->myKey;
+              strcpy(server->doubleNextIp, server->myIp);
+              strcpy(server->doubleNextPort, server->myPort);
+
+              sscanf(buffer, "%s %d %s %s", str, &(server->nextKey), server->nextIp, server->nextPort);
+              server->nextConnFD = connectToNextServer(server);
+              n = write(server->nextConnFD, "SUCCCONF\n", 10);
+
+              sprintf(str, "SUCC %d %s %s\n", server->nextKey, server->nextIp, server->nextPort);
+              n = write(server->prevConnFD, str, strlen(str));
+            } else {
+
+            }
           } else if(strstr(buffer, "SUCC ") != NULL) {
-              if((server->prevConnFD == 0) ) {
-                write(1,"\nReceived SUCC: ",17); write(1,buffer,n);
-                sscanf(buffer, "%s %d %s %s", str, &(server->doubleNextKey), server->doubleNextIp, server->doubleNextPort); // Get the double successor details and update   
-                server->prevConnFD = server->nextConnFD;
-              } else if(newfd == server->nextConnFD) {
-                sscanf(buffer, "%s %d %s %s", str, &(server->doubleNextKey), server->doubleNextIp, server->doubleNextPort); // Get the successor details
-              }
-          } else if(strcmp(buffer, "SUCCCONF\n") == 0) {
-            server->prevConnFD = newfd;
-            //n = write(server->prevConnFD, "Sucessfully connected\n", 23);
-            n = write(1, "Sucessfully connected\n", 23);
+              sscanf(buffer, "%s %d %s %s", str, &(server->doubleNextKey), server->doubleNextIp, server->doubleNextPort);
+          } else if(strstr(buffer, "SUCCCONF") != NULL) {
+              server->prevConnFD = newfd;
           }
         }
-        printServerData(server);
       }
-      printf("End for %d %d\n", i, client_sockets[i]);
     }
-    printf("Out of the for\n");
+    printServerData(server);
   }
-  freeaddrinfo(res); 
-  close(fd);
+
+for(int i=0; i<3; i++) {
+  close(client_sockets[i]);
+}
+
+freeaddrinfo(res); 
+close(fd);
+
+
 }
 
 //For handling the entry of a new server to the ring
 void serverIsEntering(char buffer[128], int *newfd, Server *server) {
-  char str[100];
-  char newIp[30];
-  char newPort[10];
-  int newKey = 0;
-  sscanf(buffer, "%s %d %s %s", str, &newKey, newIp, newPort);
-
-  if(*newfd == server->nextConnFD) { //Se o comando NEW vier do sucessor...
-      //Atualiza o meu duplo sucessor para ser o que é o meu atual sucessor
-      server->doubleNextKey = server->nextKey;
-      strcpy(server->doubleNextIp, server->nextIp);
-      strcpy(server->doubleNextPort, server->nextPort);
-
-      //Atualiza o seu sucessor para ser o servidor entrante
-      server->nextKey = newKey;
-      strcpy(server->nextIp, newIp);
-      strcpy(server->nextPort, newPort);
-      if(server->nextConnFD != server->prevConnFD) {
-        close(server->nextConnFD);
-      }
-      
-      server->nextConnFD = connectToNextServer(server); //Tem de alterar a conexão com o sucessor para se ligar ao servidor entrante
-      //Envia SUCCCONF ao servidor entrante
-      n = write(server->nextConnFD, "SUCCCONF\n", 10);  if(n==-1)/*error*/exit(1);
-
-      //Envia SUCC ao meu predecessor
-      sprintf(str, "SUCC %d %s %s\n", server->nextKey, server->nextIp, server->nextPort); //Envia SUCC ao seu predecessor (3) para ele atualizar o seu duplo sucessor
-      n = write(server->prevConnFD, str, strlen(str)); if(n==-1)/*error*/exit(1);
-  }else { //Então é porque é um novo servidor a enviar o comando NEW
-    if(server->nextConnFD == 0) { //Não tem sucessor, logo sou o unico servidor do anel
-      server->nextConnFD = *newfd; // Set the incoming request as the next server
-      server->prevConnFD = *newfd;
-      sscanf(buffer, "%s %d %s %s", str, &(server->nextKey), server->nextIp, server->nextPort); // Get the successor details
-      // Set the double next info as your own
-      strcpy(server->doubleNextIp, server->myIp);
-      strcpy(server->doubleNextPort, server->myPort);
-      server->doubleNextKey = server->myKey;
-
-      //n = write(*newfd,buffer, strlen(buffer)); if(n==-1)/*error*/exit(1);
-    } else { 
-        if(server->doubleNextKey == server->myKey) { //Se eu for o meu proprio duplo sucessor entao existem apenas 2 servidores no anel[tenho de mudar o meu proprio duplo]
-          server->doubleNextKey = newKey;
-          strcpy(server->doubleNextIp, newIp);
-          strcpy(server->doubleNextPort, newPort);
-        }
-        n = write(server->prevConnFD, buffer, strlen(buffer)); if(n==-1)/*error*/exit(1); //Enviar NEW ao seu predecessor atual (5)
-        printf("Linha 184:%ld %s \n",n, buffer);
-        //Atualiza o seu predecessor
-        server->prevConnFD = *newfd;
-        server->prevKey = newKey;
-        strcpy(server->prevIp, newIp);
-        strcpy(server->prevPort, newPort);
-    }
-      //Envia SUCC ao servidor entrante
-      sprintf(str, "SUCC %d %s %s\n", server->nextKey, server->nextIp, server->nextPort);
-      n = write(*newfd,str, strlen(str)); if(n==-1)/*error*/exit(1);
-  }
-
+  
 }
 
 int connectToNextServer(Server* server) {
@@ -268,26 +196,6 @@ int connectToNextServer(Server* server) {
   return fd;
 }
 
-
-
-//Returns key, ip and tcp from a message string like "NEW i i.IP i.TCP"
-void decodeMessage(char* message, int* key, char ip[16], char port[10]) {
-  // Returns first token 
-  char* token = strtok(message, " ");
-  int i = 0; 
-
-  // Keep printing tokens while one of the 
-  // delimiters present in str[]. 
-  while (token != NULL) { 
-      if(i==1) *key = atoi(token);
-      else if(i==2) strcpy(ip, token);
-      else if(i==3) strcpy(port, token);
-
-      token = strtok(NULL, " "); 
-      i++;
-  } 
-}
-
 void printServerData(Server* server) {
-  printf("\n\n myKey: %d\nnextKey: %d\ndoubleNextKey: %d\nprevKey: %d\nnextConnection: %d\nprevConnection: %d\n\n", server->myKey, server->nextKey, server->doubleNextKey, server->prevKey, server->nextConnFD, server->prevConnFD);
+  printf("\n\n myKey: %d\nnextKey: %d\ndoubleNextKey: %d\nnextConnection: %d\nprevConnection: %d\n\n", server->myKey, server->nextKey, server->doubleNextKey, server->nextConnFD, server->prevConnFD);
 }
